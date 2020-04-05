3f552589536f2a35bbeac0a256723142f1a8d18e
Marck Guzman <marck.guzman.dev@gmail.com>
2020-04-05 20:08:33 +0200

    componente de servicios terminado

--- components/services/services.js
+++ components/services/services.js
@@ -1,7 +1,6 @@
 // Archivo js 
 var servR = document.getElementById("services_hr");
 var servT = document.getElementById("services").scrollHeight + (window.innerHeight * 0.60);
-console.log(down)
 document.addEventListener("scroll", function(ev){
     var top = window.scrollY;
     if(top >= servT){
